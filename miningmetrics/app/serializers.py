2d2100330cacd87ac827b2f254df29927b40c965
progrsv <likeprogrsv@gmail.com>
2023-03-14 00:13:50 +0600

    all functionality for tables

--- miningmetrics/app/serializers.py
+++ miningmetrics/app/serializers.py
@@ -4,18 +4,22 @@ from .models import ConcentrateQuality, ReportDate
 
 class ConcentrateQualitySerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    report_month = serializers.PrimaryKeyRelatedField(queryset=ReportDate.objects.all())
 
     class Meta:
         model = ConcentrateQuality
         fields = [
-            'user', 'time_create', 'material_name',
-            'ferrum', 'silicium', 'aluminum',
-            'calcium', 'sulfur', ]
+            'user', 'time_create',
+            'material_name', 'ferrum', 'silicium',
+            'aluminum', 'calcium', 'sulfur', 'report_month'
+        ]
+        
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        representation['time_create'] = instance.time_create.strftime('%Y-%m-%d %H:%M:%S')
-        return representation
+        if 'time_create' in representation:
+            representation['time_create'] = instance.time_create.strftime('%Y-%m-%d %H:%M:%S')
+            return representation
 
 
 # class ConcentrateStatisticSerializer(serializers.ModelSerializer):
--- miningmetrics/app/views.py
+++ miningmetrics/app/views.py
@@ -1,6 +1,10 @@
 from re import L
+from urllib import response
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from .models import ConcentrateQuality, ReportDate
 from .serializers import ConcentrateQualitySerializer
 from django.views.generic import TemplateView, DetailView, ListView
@@ -10,6 +14,7 @@ class ConcentrateQualityAPIList(generics.ListCreateAPIView):
     lookup_field = 'pk'
     # queryset = ConcentrateQuality.objects.all()
     serializer_class = ConcentrateQualitySerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         report_date_id = self.kwargs['pk']
@@ -17,6 +22,15 @@ class ConcentrateQualityAPIList(generics.ListCreateAPIView):
             report_month_id=report_date_id
         )
 
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.get_serializer(data=data, many=True)
+        serializer.is_valid(raise_exception=True)
+
+        for obj in serializer.validated_data:
+            ConcentrateQuality.objects.get_or_create(**obj)
+    
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ReportView(ListView):
